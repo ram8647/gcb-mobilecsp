eb6f3b44ec28d7e7f77244abf9641f42df4a9044
Ralph Morelli <ram8647@gmail.com>
2016-09-24 14:29:05 -0400

    Modified Quizly descriptions.

--- modules/teacher/student_activites.py
+++ modules/teacher/student_activites.py
@@ -39,24 +39,24 @@ class ActivityScoreParser(jobs.MapReduceJob):
         self.params = {}
         self.num_attempts_dict = { }
         self.quizly_desc = {               # instance_id:description
-            'LXgF4NO50hNM':{'desc':'Pause the Player','name':'quiz_pause_the_player'},       # Unit 2
-            'BtQ8hSoGkeml':{'desc':'Stop the Player','name':'quiz_button_click_stop_player'},
-            'Dstsv7VuDQb5':{'desc':'Stop Player if playing','name':'quiz_stop_player_if_playing'},
-            'twxBgieSEwqs':{'desc':'If/else stop/start Player','name':'quiz_if_else_stop_start_player'},
-            'a3uBZXYSOJee':{'desc':'Set background color','name':'quiz_background_color'},   # Unit 3
-            'pnhvzarYPPW1':{'desc':'Set text color','name':'quiz_text_color'},
-            'G3qzTftPYKTe':{'desc':'Increment a variable','name':'quiz_incrementing'},
-            '4kITN7u5hdsO':{'desc':'Initialize global variable','name':'quiz_initialize_variable'},
-            'pCZugPUxlHeb':{'desc':'Initializing','name':'quiz_incrementing'},
-            '8T30OkUf5r1r':{'desc':'Simple if/else','name':'quiz_simple_if_else'},
-            'KQctST8skmaC':{'desc':'Procedure to double a variable','name':'quiz_proc_double'},
-            'v2m4Ks25S1MX':{'desc':'Procedure to add globals','name':'quiz_add_global'},
-            'rCgLbJRceEbn':{'desc':'Procedure to reset the score','name':'quiz_reset_score'},     # Unit 4   
-            '7uowepixSjT4':{'desc':'Procedure to calculate the hit rate','name':'quiz_calculate_hit_rate'},
-            'w18q4UWKxvlM':{'desc':'Fix a bug in updateScore procedure','name':'quiz_procedure_bug'},
-            'rvjUJMaLZ56s':{'desc':'If/else greater than','name':'quiz_if_x_greater_than_y'},
-            'scgF2VSCjUv8':{'desc':'Simple if/else','name':'quiz_simple_if_else'},    # Unit 5
-            'JatcV7u6GOer':{'desc':'If x greater than y','name':'quiz_if_x_greater_than_y'},   
+            'LXgF4NO50hNM':{'desc':'Quizly, Pause the Player','name':'quiz_pause_the_player'},       # Unit 2
+            'BtQ8hSoGkeml':{'desc':'Quizly, Stop the Player','name':'quiz_button_click_stop_player'},
+            'Dstsv7VuDQb5':{'desc':'Quizly, Stop Player if playing','name':'quiz_stop_player_if_playing'},
+            'twxBgieSEwqs':{'desc':'Quizly, If/else stop/start Player','name':'quiz_if_else_stop_start_player'},
+            'a3uBZXYSOJee':{'desc':'Quizly, Set background color','name':'quiz_background_color'},   # Unit 3
+            'pnhvzarYPPW1':{'desc':'Quizly, Set text color','name':'quiz_text_color'},
+            'G3qzTftPYKTe':{'desc':'Quizly, Increment a variable','name':'quiz_incrementing'},
+            '4kITN7u5hdsO':{'desc':'Quizly, Initialize global variable','name':'quiz_initialize_variable'},
+            'pCZugPUxlHeb':{'desc':'Quizly, Initializing','name':'quiz_incrementing'},
+            '8T30OkUf5r1r':{'desc':'Quizly, Simple if/else','name':'quiz_simple_if_else'},
+            'KQctST8skmaC':{'desc':'Quizly, Procedure to double a variable','name':'quiz_proc_double'},
+            'v2m4Ks25S1MX':{'desc':'Quizly, Procedure to add globals','name':'quiz_add_global'},
+            'rCgLbJRceEbn':{'desc':'Quizly, Procedure to reset the score','name':'quiz_reset_score'},     # Unit 4   
+            '7uowepixSjT4':{'desc':'Quizly, Procedure to calculate the hit rate','name':'quiz_calculate_hit_rate'},
+            'w18q4UWKxvlM':{'desc':'Quizly, Fix a bug in updateScore procedure','name':'quiz_procedure_bug'},
+            'rvjUJMaLZ56s':{'desc':'Quizly, If/else greater than','name':'quiz_if_x_greater_than_y'},
+            'scgF2VSCjUv8':{'desc':'Quizly, Simple if/else','name':'quiz_simple_if_else'},    # Unit 5
+            'JatcV7u6GOer':{'desc':'Quizly, If x greater than y','name':'quiz_if_x_greater_than_y'},   
             #  No Quizly exercises beyond Unit 5 -->   we should create more!
         }
 
